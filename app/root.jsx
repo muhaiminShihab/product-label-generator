563f0bbcfb82f89b2a7e3c815d5d8329a2657a8f
Md Muhaiminul Islam Shihab <talk.shihab@gmail.com>
2025-03-07 00:17:49 +0600

    add ErrorBoundary.

--- app/root.jsx
+++ app/root.jsx
@@ -4,7 +4,32 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>
+          {isRouteErrorResponse(error)
+            ? `${error.status} ${error.statusText}`
+            : error instanceof Error
+            ? error.message
+            : "Unknown Error"}
+        </h1>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
 
 export default function App() {
   return (
@@ -12,6 +37,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="description" content="Product Label Generator" />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
         <link
           rel="stylesheet"
